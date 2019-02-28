3b205408058ca9620d0b2badf7a75a9bda0ae715
Jack Massey <jacknmassey@gmail.com>
2019-02-28 10:34:10 +1000

    Fixed formatting

--- include/spi.h
+++ include/spi.h
@@ -1,21 +1,21 @@
 #ifndef _SPI_H_
 #define _SPI_H_
 
-#include<avr/io.h>
+#include <avr/io.h>
 
 void spi_init(void);
 
 static inline void spi_write(uint8_t byte) {
-	SPDR = byte;
-	while(!(SPSR & (1<<SPIF)));
+    SPDR = byte;
+    while (!(SPSR & (1<<SPIF)));
 }
 
 static inline void spi_set_cs(void) {
-	PORTB |= (1 << PB2);
+    PORTB |= (1 << PB2);
 }
 
 static inline void spi_unset_cs(void) {
-	PORTB &= ~(1 << PB2);
+    PORTB &= ~(1 << PB2);
 }
 
 
--- main.c
+++ main.c
@@ -15,23 +15,47 @@ int main(void) {
     st7735_init();
 
     st7735_set_orientation(ST7735_LANDSCAPE);
-    st7735_fill_rect(        0, 0, 128, 128, ST7735_COLOR_WHITE);
 
-    st7735_draw_text(        5, 12, "ST7735 AVR Library", &TomThumb, 1, ST7735_COLOR_BLACK);
-    st7735_draw_line(        5, 13, 70, 13, ST7735_COLOR_BLACK);
-    st7735_draw_line(        5, 15, 70, 15, ST7735_COLOR_BLACK);
-    st7735_draw_text(        5, 30, "bitmaps:", &TomThumb, 1, ST7735_COLOR_BLACK);
-    st7735_draw_mono_bitmap(45, 25, (PGM_P) test_bw, ST7735_COLOR_WHITE, ST7735_COLOR_BLACK);
-    st7735_draw_bitmap(     80, 25, (PGM_P) test_colour);
-    st7735_draw_text(        5, 72, "Circles:", &TomThumb, 1, ST7735_COLOR_BLACK);
-    st7735_draw_circle(     45, 69, 5, ST7735_COLOR_RED);
-    st7735_draw_text(        5, 93, "Lines:", &TomThumb, 1, ST7735_COLOR_BLACK);
+    // Reset with all white
+    st7735_fill_rect(0, 0, 128, 128, ST7735_COLOR_WHITE);
+
+    // Draw title
+    st7735_draw_text(
+        5,
+        12,
+        "ST7735 AVR Library",
+        &TomThumb,
+        1,
+        ST7735_COLOR_BLACK
+    );
+    st7735_draw_line(5, 13, 70, 13, ST7735_COLOR_BLACK);
+    st7735_draw_line(5, 15, 70, 15, ST7735_COLOR_BLACK);
+
+    // Draw bitmap examples
+    st7735_draw_text(5, 30, "bitmaps:", &TomThumb, 1, ST7735_COLOR_BLACK);
+    st7735_draw_mono_bitmap(
+        45,
+        25,
+        (PGM_P) test_bw,
+        ST7735_COLOR_WHITE,
+        ST7735_COLOR_BLACK
+    );
+    st7735_draw_bitmap(80, 25, (PGM_P) test_colour);
+
+    // Draw circle example
+    st7735_draw_text(5, 72, "Circles:", &TomThumb, 1, ST7735_COLOR_BLACK);
+    st7735_draw_circle(45, 69, 5, ST7735_COLOR_RED);
+
+    // Draw line example
+    st7735_draw_text(5, 93, "Lines:", &TomThumb, 1, ST7735_COLOR_BLACK);
     for(uint8_t y = 85; y <= 95; y += 5) {
-        st7735_draw_line(   35, 90, 80, y, ST7735_COLOR_GREEN);
+        st7735_draw_line(35, 90, 80, y, ST7735_COLOR_GREEN);
     }
-    st7735_draw_text(        5, 112, "Rectangles:", &TomThumb, 1, ST7735_COLOR_BLACK);
-    st7735_draw_rect(       55, 105, 10, 10, ST7735_COLOR_BLACK);
-    st7735_fill_rect(       70, 105, 10, 10, ST7735_COLOR_BLUE);
+
+    // Draw rectangle examples
+    st7735_draw_text(5, 112, "Rectangles:", &TomThumb, 1, ST7735_COLOR_BLACK);
+    st7735_draw_rect(55, 105, 10, 10, ST7735_COLOR_BLACK);
+    st7735_fill_rect(70, 105, 10, 10, ST7735_COLOR_BLUE);
 
     while(1);
 }
